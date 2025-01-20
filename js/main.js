065530ea67b70cc1f6f4752fdf73ed549cc32e69
Sekwaticode <lsekwati2@gmail.com>
2025-01-20 14:10:49 +0200

    video modal and gallery

--- js/main.js
+++ js/main.js
@@ -109,6 +109,24 @@
             '<i class="fa fa-angle-right" aria-hidden="true"></i>'
         ],
     });
+
+    
+        // Modal Video
+        $(document).ready(function () {
+            var $videoSrc;
+            $('.btn-play').click(function () {
+                $videoSrc = $(this).data("src");
+            });
+            console.log($videoSrc);
+
+            $('#videoModal').on('shown.bs.modal', function (e) {
+                $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+            })
+
+            $('#videoModal').on('hide.bs.modal', function (e) {
+                $("#video").attr('src', '');
+            })
+        });
     
 })(jQuery);
 
